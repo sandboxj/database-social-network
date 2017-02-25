da572f75f397ca6569a0c7edce74f37213988d0b
gendi93 <yehia.gendi@gmail.com>
2017-02-25 11:08:13 +0000

    Update navbar.php

--- userarea/navbar.php
+++ userarea/navbar.php
@@ -1,8 +1,12 @@
-<nav class="navbar-container">
+<link rel="stylesheet" href="../styles/fonts">
+  <nav class="navbar-container">
   <ul class="nav nav-pills">
     <li><span><a class="navbar-links" href="profile.php">Profile</a></span></li>
-    <li><span><a class="navbar-links" href="blog.php">Blog</a></span></li>
-    <li><span><a class="navbar-links" href="collections.php">Photos</a></span></li>
+    <li><span><a class="navbar-links" href="blog.php">Blogs</a></span></li>
+    <li><span><a class="navbar-links" href="collections.php">Collections</a></span></li>
     <li><span><a class="navbar-links" href="circles.php">Circles</a></span></li>
+    <li><span><a class="navbar-links" href="messages.php">Messages</a></span></li>
+    <li><span><a class="navbar-links" href="friends.php">Friends</a></span></li>
+  <span><a href="search.php"><i class="glyphicon glyphicon-search" aria-hidden="true"></i></a></span>
   </ul>
 </nav>
