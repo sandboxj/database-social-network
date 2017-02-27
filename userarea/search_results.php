50513272c2a878f9b252aa12bbfc34a107635ae1
gendi93 <yehia.gendi@gmail.com>
2017-02-27 22:10:16 +0000

    Update search_results.php

--- userarea/search_results.php
+++ userarea/search_results.php
@@ -1,14 +1,28 @@
 <?php require_once("../server/sessions.php"); ?>
 <?php require_once("../server/functions.php");?>
 <?php require_once("../server/db_connection.php");?>
+<?php require_once("../server/validation_search.php");?>
 <?php $page_title="Search results"?>
 <?php confirm_logged_in(); ?>
 <?php include("../includes/header.php"); ?>
 <?php include("navbar.php"); ?>
 
 		<h2>Search results</h2>
-		<!--Insert code here-->
 		
+    <?php echo message()?>
+    <?php
+    while($u = mysqli_fetch_assoc($result)) {
+        $pic = find_profile_pic($u["UserID"]);
+        while($picture = mysqli_fetch_assoc($pic)) {
+            $out = "../userarea/img/" . $picture["FileSource"];
+        }
+        $image = "<td><a href='user_profile.php?id={$u["UserID"]}'><img src={$out} class=\"img-rounded\" alt=\"Cinque Terre\" width=\"304\" height=\"236\"></a></td><br/>";
+        echo $image;
+        $output = "<td><a href='user_profile.php?id={$u["UserID"]}'>" . $u["FirstName"] . " " . $u["LastName"] . "</a></td>";
+        echo "<h4>" . $output . "</h4>";
+    }
+    mysqli_free_result($result);
+		?>
 		<hr />
 		<a href="logout.php">Logout</a>
 
