b525f1376edcd2ea20c2f2761923c8c956d4c433
tvaupel <tvaupel@users.noreply.github.com>
2017-03-02 23:32:07 +0000

    Updated the Navigation Bar (#39)
    * Updated the Navigation Bar
    
    * Changed error for navbar.php in line 18

--- userarea/navbar.php
+++ userarea/navbar.php
@@ -1,15 +1,20 @@
-
 <link rel="stylesheet" href="../styles/fonts">
-  <nav class="navbar-container">
-  <ul class="nav nav-pills">
-    <li><span><a class="navbar-links" href="profile.php">Profile</a></span></li>
-    <li><span><a class="navbar-links" href="blogs.php">Blogs</a></span></li>
-    <li><span><a class="navbar-links" href="collections.php">Photos</a></span></li>
-    <li><span><a class="navbar-links" href="circles.php">Circles</a></span></li>
-    <li><span><a class="navbar-links" href="messages.php">Messages</a></span></li>
-    <li><span><a class="navbar-links" href="friends.php">Friends</a></span></li>
-  <span><a href="search.php"><i class="glyphicon glyphicon-search" aria-hidden="true"></i></a></span>
 
-  </ul>
-
+<nav class="navbar-container">
+    <ul class="nav nav-pills">
+        <li role="presentation" <?php if ($page_title == "{$_SESSION["FirstName"]} {$_SESSION["LastName"]}'s Profile") { ?> class="active" <?php }; ?> >
+            <a href="profile.php">Profile</a></li>
+        <li role="presentation" <?php if ($page_title == "{$_SESSION["FirstName"]} {$_SESSION["LastName"]}'s Blogs") { ?> class="active" <?php }; ?> >
+            <a href="blogs.php">Blog</a></li>
+        <li role="presentation" <?php if ($page_title == "Photo Collections") { ?> class="active" <?php }; ?> ><a
+                    href="collections.php">Photos</a></li>
+        <li role="presentation" <?php if ($page_title == "Circles") { ?> class="active" <?php }; ?> ><a
+                    href="circles.php">Circles</a></li>
+        <li role="presentation" <?php if ($page_title == "Messages") { ?> class="active" <?php }; ?> ><a
+                    href="messages.php">Messages</a></li>
+        <li role="presentation" <?php if ($page_title == "{$_SESSION["FirstName"]} {$_SESSION["LastName"]}'s Friends") { ?> class="active" <?php }; ?> >
+            <a href="friends.php">Friends</a></li>
+        <li role="presentation" <?php if ($page_title == "Search") { ?> class="active" <?php }; ?> ><a
+                    href="search.php"><i class="glyphicon glyphicon-search" aria-hidden="true"></i></a></li>
+    </ul>
 </nav>
